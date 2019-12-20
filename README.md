db6737db06c82bed6248ceac27b3ac26b356ff80
Kina Rain <tina@lawtk.net>
2019-12-19 22:52:46 -0500

    finished profile generator project

--- README.md
+++ README.md
@@ -1,66 +1,21 @@
-# ProfileGenerator
-API calls to Github
+# Profile Generator
+    This project involved using API calls to Github to make a PDF profile from a GitHub username. 
+The user is prompted with a couple questions, so a PDF profile can be populated. The PDF contains the
+following: User name, image, bio, repositories, followers, stars, and users followings. There were also 
+links to location, profile, and users blog.
 
-copied Read Me:
-# Unit 09 Node.js and ES6+ Homework: Developer Profile Generator
+## Screen Shot
 
-Create a command-line application that dynamically generates a PDF profile from a GitHub username. The application will be invoked with the following command:
+*Link: https://github.com/Kina-rain/Portfolio-Kina
 
-```sh
-node index.js
-```
+![alt text][screenshot]
 
-The user will be prompted for a favorite color, which will be used as the background color for cards.
+[screenshot]: https://github.com/Kina-rain/ProfileGenerator/blob/master/screenShot.png "Portfolio Generator"
 
-The PDF will be populated with the following:
 
-* Profile image
-* User name
-* Links to the following:
-  * User location via Google Maps
-  * User GitHub profile
-  * User blog
-* User bio
-* Number of public repositories
-* Number of followers
-* Number of GitHub stars
-* Number of users following
-
-Following the [common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates), we can frame this challenge as follows:
-
-```
-AS A product manager
-
-I WANT a developer profile generator
-
-SO THAT I can easily prepare reports for stakeholders
-```
-
-Refer to the [design mockup](./Assets/09-NodeJS-homework-demo.pdf).
-
-## Business Context
-
-When preparing a report for stakeholders, it is important to have up-to-date information about members of the development team. Rather than navigating to each team member's GitHub profile, a command-line application will allow for quick and easy generation of profiles in PDF format.
-
-## Minimum Requirements
-
-* Functional, deployed application.
-
-* GitHub repository with a unique name and a README describing project.
-
-* The application generates a PDF resume from the user provided GitHub profile.
-
-* The generated resume includes a bio image from the user's GitHub profile.
-
-* The generated resume includes the user's location and a link to their GitHub profile.
-
-* The generated resume includes the number of: public repositories, followers, GitHub stars and following count.
-
-* The background color of the generated PDF matches the color that the user provides.
-
-```
-GIVEN the developer has a GitHub profile
-
-WHEN prompted for the developer's GitHub username and favorite color
-
-THEN a PDF profile is generated
+## References
+Phantom-html2pdf for NodeJS
+npmjs.com
+w3schools
+blog.logrocket.com (axios examples)
+stackoverflow.com
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const pdf = require("html-pdf");
-const options = { format: "Letter" };
+const pdf = require("phantom-html2pdf");
 
-
+//questions for users to answer
 const questions = [
     {
         type: "input",
@@ -13,60 +12,219 @@ const questions = [
     },
     {
         type: "input",
-        message: "What is your favorite color?",
+        message: "What is your favorite color other than black?",
         name: "color"
     }
 ]
 
+//fire up the console interface
 inquirer
     .prompt(questions)
     .then(function (response) {
-        //  console.log(response.username)
-        //  console.log(response.color)
-        //   console.log(JSON.stringify(response))
-        //   console.log(generateHTML(response))
 
-        axios.get(`https://api.github.com/users/${response.username}/repos?per_page=100`).then(res => {
+        //setup the file names
+        let htmlFilename = `${response.username}_profile.html`;
+        let pdfFilename = `${response.username}_profile.pdf`;
 
-            axios.get(`https://api.github.com/users/${response.username}/watched`).then(result => {
-                let starCount = result.stargazers_count;
-            });
+        //Perform the ajax calls and generate the html/pdf from the data
+        axios.all([
+            axios.get(`https://api.github.com/users/${response.username}`),
+            axios.get(`https://api.github.com/users/${response.username}/watched`)
+        ])
+            .then(responseArr => {
+                let gitHubMainResponse = responseArr[0].data;
+                let starCount = getStarCount(responseArr[1].data);
 
-            //Call Generate HTML
-            let filename = `${response.username}_profile.html`
+                //write the html file from the response data
+                fs.writeFile(htmlFilename, generateHTML(response, gitHubMainResponse, starCount), function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
 
-            fs.writeFile(filename, generateHTML(response, res, starCount), function (err) {
-                if (err) {
-                    return console.log(err);
+                //These are the options that phantom-html2pdf needs to write the file
+                let options = {
+                    "html": `${htmlFilename}`,
+                    "css": "",
+                    "js": "",
+                    "runnings": "",
+                    "paperSize": { format: 'A4', orientation: 'portrait', border: '1cm' },
+                    "deleteOnAction": "true",
+                    "runningsArgs": ""
                 }
-                console.log("Wrote " + filename)
-            });
 
-            let html = fs.readFileSync(filename, "utf8");
-            pdf.create(html, options).toFile(`./${response.username}_profile.pdf`, function(err, res) {
-                if (err) return console.log(err);
+                //run the convert method on the html file to make a pdf
+                pdf.convert(options, function (err, result) {
+                    result.toFile(pdfFilename, function () { });
+                });
             });
-        });
     });
 
+function getStarCount(gitData) {
 
+    //counter for stars
+    let sCount = 0;
+
+    //using map to get the stargazers_count from the array
+    const map1 = gitData.map(gitInfo => sCount += gitInfo.stargazers_count);
+
+    //return the last element in the array since that is the max star count
+    return map1[map1.length - 1];
+}
 
 function generateHTML(answers, githubResponse, starCount) {
+    //Here I am generating and return formated HTML to produce a professional profile page.
     return `<!DOCTYPE html>
     <html lang='en'>
+    
     <head>
         <meta charset='UTF-8'>
         <meta name='viewport' content="width=device-width, initial-scale=1">
-        <link href="assets/css/style.css" rel="stylesheet">
+        <title>Profile</title>
         <style>
-            body {
-                background-color: ${answers.color}
+            .footer {
+                height: 80px;
+                background-color: ${answers.color};
+                margin: 0 auto;
+                border-radius: 25px;
+                width: 700px;
+            }
+    
+            .profileImg {
+                border-radius: 75px;
+                float: center;
+                padding-top: 10px;
+            }
+    
+            .backgroundCard {
+                background-color: beige;
+                width: 700px;
+                height: 450px;
+                position: relative;
+                margin: 0 auto;
+                border-radius: 25px;
+            }
+    
+            .bigCard {
+                background-color: ${answers.color};
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                margin: 10px auto;
+                text-align: center;
+                height: 400px;
+                width: 390px;
+                z-index: 1;
+                border-radius: 25px;
+            }
+    
+            .bio {
+                width: 80%;
+                margin: 0 auto;
+                border: 2px solid ${answers.color};
+                border-radius: 25px;
+                text-align: center;
+            }
+    
+            .wrapper {
+                margin: 0 auto;
+                width: 48%;
+                text-align: center;
+            }
+    
+            .topText {
+                background-color: ${answers.color};
+                border: 2px solid black;
+                border-top-left-radius: 25px;
+                border-top-right-radius: 25px;
+                text-align: center;
+                font-size: 26px;
+                font-weight: bold;
+                margin-bottom: 0;
+                width: 250px;
+            }
+    
+            .bottomText {
+                border: 2px solid black;
+                border-bottom-left-radius: 25px;
+                border-bottom-right-radius: 25px;
+                text-align: center;
+                font-size: 20px;
+                margin-top: 0;
+            }
+    
+            .parent {
+                margin-right: 10px;
+                display: inline-block;
+                width: 48%;
+            }
+    
+            .child {
+                display: inline-block;
+                margin-right: 5px;
+            }
+    
+            a {
+                color: black;
             }
         </style>
-        <title>${answers.username} Resume Profile</title>
+        <script src="https://kit.fontawesome.com/78debb8d07.js" crossorigin="anonymous"></script>
     </head>
+    
     <body>
-        <h1>${answers.username}</h1>
+    
+        <body>
+            <div class="backgroundCard">
+                <div class="bigCard">
+                    <img class="profileImg" src="${githubResponse.avatar_url}" alt="" height="190px" width="200px">
+                    <h1>Hello, My Name is ${answers.username}</h1>
+                    <h3>Current at: </h3>
+                    <h4>
+                        <a href="https://www.google.com/maps/search/?api=1&query=${githubResponse.location}"><i class="fas fa-globe-americas"></i>  Location   </a>
+                        <a href="${githubResponse.html_url}"><i class="fab fa-github"></i>  GitHub   </a>
+                        <a href="${githubResponse.blog}"><i class="fas fa-rss"></i>  Blog</a>
+                    </h4>
+                    <!-- Main header line -->
+                </div>
+            </div>
+    
+            <div class="bio">
+                <h4>${githubResponse.bio}</h4>
+            </div>
+    
+            <!-- 2 Boxes to put info in 1st Row -->
+            <div class="wrapper">
+                <div class="parent">
+                    <div class="child">
+                        <p class="topText">Public Repositories</p>
+                        <p class="bottomText">${githubResponse.public_repos}</p>
+                    </div>
+                </div>
+                <div class="parent">
+                    <div class="child">
+                        <p class="topText">Followers</p>
+                        <p class="bottomText">${githubResponse.followers}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="parent">
+                    <div class="child">
+                        <p class="topText">Git Hub Stars</p>
+                        <p class="bottomText">${starCount}</p>
+                    </div>
+                </div>
+                <div class="parent">
+                    <div class="child">
+                        <p class="topText">Following</p>
+                        <p class="bottomText">${githubResponse.following}</p>
+                    </div>
+                </div>
+            </div>
+        </body>
     </body>
+    
     </html>`
 }
